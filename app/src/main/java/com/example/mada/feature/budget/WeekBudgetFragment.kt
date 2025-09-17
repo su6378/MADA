be89a4bed1f866fde40ff8e18414cb6fa3c29cf1
SooYun Jo <josooyeon11@naver.com>
2025-09-17 15:57:37 +0900

    [FEAT] 예산 설정 기능 추가
    - 마이데이터 자산 연동 기능
    - 입력한 예산 설정시 예산 데이터에 저장

--- app/src/main/java/com/example/mada/feature/budget/WeekBudgetFragment.kt
+++ app/src/main/java/com/example/mada/feature/budget/WeekBudgetFragment.kt
@@ -1,15 +1,25 @@
 package com.example.mada.feature.budget
 
-import android.content.res.ColorStateList
+import android.annotation.SuppressLint
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.example.mada.MainActivity
 import com.example.mada.R
 import com.example.mada.base.BaseFragment
 import com.example.mada.databinding.FragmentWeekBudgetBinding
+import com.example.mada.dialog.AlertDialog
+import com.example.mada.dialog.LoadingDialog
+import com.example.mada.feature.home.HomeFragmentDirections
+import com.example.mada.util.TextUtil
+import com.example.mada.util.TextUtil.toWon
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -20,6 +30,13 @@ class WeekBudgetFragment : BaseFragment<FragmentWeekBudgetBinding, WeekBudgetVie
     override val layoutResourceId: Int
         get() = R.layout.fragment_week_budget
     override val viewModel: WeekBudgetViewModel by viewModels()
+
+    private lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
 
     override fun initView() {
         with(binding) {
@@ -35,33 +52,79 @@ class WeekBudgetFragment : BaseFragment<FragmentWeekBudgetBinding, WeekBudgetVie
                 backNavigate()
             }
 
+            focusEditText()
+            setAmountBudget()
+
             toggleCalendar.setOnCheckedChangeListener { toggle, isChecked -> // 캘린더 연동
 
                 if (isChecked) {
                     tvWeekBudgetComment.text = resources.getString(R.string.week_budget_comment2)
-                    tvWeekBudgetComment.setTextColor(ContextCompat.getColor(requireContext(),R.color.orange))
+                    tvWeekBudgetComment.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.orange
+                        )
+                    )
                     tvWeekBudgetFridayComment.visibility = View.VISIBLE
                     cvWeekBudgetFriday.visibility = View.VISIBLE
-                    tvWeekBudgetFridayBudget.setTextColor(ContextCompat.getColor(requireContext(),R.color.orange))
+                    etWeekBudgetFridayBudget.setText(55000.toWon())
+                    etWeekBudgetFridayBudget.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.orange
+                        )
+                    )
                     tvWeekBudgetSaturdayComment.visibility = View.VISIBLE
                     cvWeekBudgetSaturday.visibility = View.VISIBLE
-                    tvWeekBudgetSaturdayBudget.setTextColor(ContextCompat.getColor(requireContext(),R.color.orange))
+                    etWeekBudgetSaturdayBudget.setText(80000.toWon())
+                    etWeekBudgetSaturdayBudget.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.orange
+                        )
+                    )
                     tvWeekBudgetSundayComment.visibility = View.VISIBLE
                     cvWeekBudgetSunday.visibility = View.VISIBLE
-                    tvWeekBudgetSundayBudget.setTextColor(ContextCompat.getColor(requireContext(),R.color.orange))
+                    etWeekBudgetSundayBudget.setText(40000.toWon())
+                    etWeekBudgetSundayBudget.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.orange
+                        )
+                    )
 
-                }else {
+                } else {
                     tvWeekBudgetComment.text = resources.getString(R.string.week_budget_comment)
-                    tvWeekBudgetComment.setTextColor(ContextCompat.getColor(requireContext(),R.color.nh_green))
+                    tvWeekBudgetComment.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.nh_green
+                        )
+                    )
                     tvWeekBudgetFridayComment.visibility = View.GONE
-                    tvWeekBudgetFridayBudget.setTextColor(ContextCompat.getColor(requireContext(),R.color.nh_green))
+                    etWeekBudgetFridayBudget.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.nh_green
+                        )
+                    )
                     cvWeekBudgetFriday.visibility = View.INVISIBLE
                     tvWeekBudgetSaturdayComment.visibility = View.GONE
                     cvWeekBudgetSaturday.visibility = View.INVISIBLE
-                    tvWeekBudgetSaturdayBudget.setTextColor(ContextCompat.getColor(requireContext(),R.color.nh_green))
+                    etWeekBudgetSaturdayBudget.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.nh_green
+                        )
+                    )
                     tvWeekBudgetSundayComment.visibility = View.GONE
                     cvWeekBudgetSunday.visibility = View.INVISIBLE
-                    tvWeekBudgetSundayBudget.setTextColor(ContextCompat.getColor(requireContext(),R.color.nh_green))
+                    etWeekBudgetSundayBudget.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.nh_green
+                        )
+                    )
                 }
 
             }
@@ -75,6 +138,20 @@ class WeekBudgetFragment : BaseFragment<FragmentWeekBudgetBinding, WeekBudgetVie
                     viewModel.action.collect { action ->
                         when (action) {
                             is WeekBudgetAction.ShowToast -> showToast(action.content)
+                            is WeekBudgetAction.ShowBudgetDialog -> {
+                                val budget = getBudgetInfo()
+
+                                showAlertDialog(
+                                    dialog = AlertDialog(
+                                        mainActivity,
+                                        title = "예산 설정",
+                                        content = "입력한 예산으로 설정하시겠어요?"
+                                    ) {
+                                        binding.vm!!.setBudgetInfo(budget)
+                                    }, viewLifecycleOwner
+                                )
+                            }
+                            is WeekBudgetAction.NavigateHomeView -> navigate(WeekBudgetFragmentDirections.actionWeekBudgetFragmentToHomeFragment())
                         }
                     }
                 }
@@ -96,11 +173,186 @@ class WeekBudgetFragment : BaseFragment<FragmentWeekBudgetBinding, WeekBudgetVie
 
                 launch {
                     viewModel.state.collect { state ->
-
+                        // 마이데이터 자산 연동
+                        if (!state.isMyDataLinked) showLoading(dialog = LoadingDialog(mainActivity))
+                        else setMyDataBudgetInfo(state)
 
                     }
                 }
             }
         }
+    }
+
+    // 마이데이터 자산 연동 값 세팅
+    private fun setMyDataBudgetInfo(state: WeekBudgetState) {
+        binding.apply {
+            if (state.budget.isNotEmpty()) {
+                tvWeekBudgetAmount.text = state.sumBudget
+                etWeekBudgetMondayBudget.setText(state.budget[0].toWon())
+                etWeekBudgetTuesdayBudget.setText(state.budget[1].toWon())
+                etWeekBudgetWednesdayBudget.setText(state.budget[2].toWon())
+                etWeekBudgetThursdayBudget.setText(state.budget[3].toWon())
+                etWeekBudgetFridayBudget.setText(state.budget[4].toWon())
+                etWeekBudgetSaturdayBudget.setText(state.budget[5].toWon())
+                etWeekBudgetSundayBudget.setText(state.budget[6].toWon())
+            }
+        }
+    }
+
+    // editText 포커스될 때 "원"이 있을시 초기화 하는 함수
+    private fun focusEditText() {
+        binding.apply {
+            etWeekBudgetMondayBudget.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && binding.etWeekBudgetMondayBudget.text.toString().contains("원")) {
+                    etWeekBudgetMondayBudget.text?.clear()
+                }
+            }
+
+            etWeekBudgetTuesdayBudget.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && binding.etWeekBudgetTuesdayBudget.text.toString().contains("원")) {
+                    etWeekBudgetTuesdayBudget.text?.clear()
+                }
+            }
+
+            etWeekBudgetWednesdayBudget.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && binding.etWeekBudgetWednesdayBudget.text.toString().contains("원")) {
+                    etWeekBudgetWednesdayBudget.text?.clear()
+                }
+            }
+
+            etWeekBudgetThursdayBudget.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && binding.etWeekBudgetThursdayBudget.text.toString().contains("원")) {
+                    etWeekBudgetThursdayBudget.text?.clear()
+                }
+            }
+
+            etWeekBudgetFridayBudget.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && binding.etWeekBudgetFridayBudget.text.toString().contains("원")) {
+                    etWeekBudgetFridayBudget.text?.clear()
+                }
+            }
+
+            etWeekBudgetSaturdayBudget.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && binding.etWeekBudgetSaturdayBudget.text.toString().contains("원")) {
+                    etWeekBudgetSaturdayBudget.text?.clear()
+                }
+            }
+
+            etWeekBudgetSundayBudget.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus && binding.etWeekBudgetSundayBudget.text.toString().contains("원")) {
+                    etWeekBudgetSundayBudget.text?.clear()
+                }
+            }
+        }
+    }
+
+    // 총 예산 실시간 반영
+    private fun setAmountBudget() {
+        binding.apply {
+            etWeekBudgetMondayBudget.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                @SuppressLint("SetTextI18n")
+                override fun afterTextChanged(s: Editable?) {
+                    val amount = getBudgetInfo().sum()
+                    tvWeekBudgetAmount.text = "총 ${amount.toWon()}"
+                }
+            })
+
+            etWeekBudgetTuesdayBudget.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                @SuppressLint("SetTextI18n")
+                override fun afterTextChanged(s: Editable?) {
+                    val amount = getBudgetInfo().sum()
+                    tvWeekBudgetAmount.text = "총 ${amount.toWon()}"
+                }
+            })
+
+            etWeekBudgetWednesdayBudget.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                @SuppressLint("SetTextI18n")
+                override fun afterTextChanged(s: Editable?) {
+                    val amount = getBudgetInfo().sum()
+                    tvWeekBudgetAmount.text = "총 ${amount.toWon()}"
+                }
+            })
+
+            etWeekBudgetThursdayBudget.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                @SuppressLint("SetTextI18n")
+                override fun afterTextChanged(s: Editable?) {
+                    val amount = getBudgetInfo().sum()
+                    tvWeekBudgetAmount.text = "총 ${amount.toWon()}"
+                }
+            })
+
+            etWeekBudgetFridayBudget.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                @SuppressLint("SetTextI18n")
+                override fun afterTextChanged(s: Editable?) {
+                    val amount = getBudgetInfo().sum()
+                    tvWeekBudgetAmount.text = "총 ${amount.toWon()}"
+                }
+            })
+
+            etWeekBudgetSaturdayBudget.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                @SuppressLint("SetTextI18n")
+                override fun afterTextChanged(s: Editable?) {
+                    val amount = getBudgetInfo().sum()
+                    tvWeekBudgetAmount.text = "총 ${amount.toWon()}"
+                }
+            })
+
+            etWeekBudgetSundayBudget.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                @SuppressLint("SetTextI18n")
+                override fun afterTextChanged(s: Editable?) {
+                    val amount = getBudgetInfo().sum()
+                    tvWeekBudgetAmount.text = "총 ${amount.toWon()}"
+                }
+            })
+        }
+    }
+
+    // 예산값 숫자로 변환
+    private fun getBudgetInfo(): IntArray {
+        val budget = IntArray(7)
+
+        binding.apply {
+            for (i in budget.indices) {
+                when (i) {
+                    0 -> budget[i] = TextUtil.getEditTextValueAsInt(etWeekBudgetMondayBudget)
+                    1 -> budget[i] = TextUtil.getEditTextValueAsInt(etWeekBudgetTuesdayBudget)
+                    2 -> budget[i] = TextUtil.getEditTextValueAsInt(etWeekBudgetWednesdayBudget)
+                    3 -> budget[i] = TextUtil.getEditTextValueAsInt(etWeekBudgetThursdayBudget)
+                    4 -> budget[i] = TextUtil.getEditTextValueAsInt(etWeekBudgetFridayBudget)
+                    5 -> budget[i] = TextUtil.getEditTextValueAsInt(etWeekBudgetSaturdayBudget)
+                    6 -> budget[i] = TextUtil.getEditTextValueAsInt(etWeekBudgetSundayBudget)
+                }
+            }
+        }
+
+        return budget
     }
 }
--- app/src/main/java/com/example/mada/feature/budget/WeekBudgetViewModel.kt
+++ app/src/main/java/com/example/mada/feature/budget/WeekBudgetViewModel.kt
@@ -1,14 +1,22 @@
 package com.example.mada.feature.budget
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.mada.feature.account.AccountAction
+import com.example.mada.repository.DataStoreRepository
+import com.example.mada.util.BudgetUtil
+import com.example.mada.util.DateUtil
+import com.example.mada.util.TextUtil.toWon
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,6 +24,7 @@ private const val TAG = "DX"
 
 @HiltViewModel
 class WeekBudgetViewModel @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository
 ) : ViewModel() {
     private val _action: MutableSharedFlow<WeekBudgetAction> = MutableSharedFlow()
     val action: SharedFlow<WeekBudgetAction> get() = _action.asSharedFlow()
@@ -26,28 +35,80 @@ class WeekBudgetViewModel @Inject constructor(
     private val _state: MutableStateFlow<WeekBudgetState> = MutableStateFlow(WeekBudgetState())
     val state: StateFlow<WeekBudgetState> get() = _state.asStateFlow()
 
-    // 질문 요청
-    fun request() {
+    init {
+        getDateInfo()
+        getMyData()
+    }
+
+    // 날짜 정보 받기
+    private fun getDateInfo() {
         viewModelScope.launch {
-            _result.emit(Result.Loading)
-
-            runCatching {
-
-            }.onSuccess { // 응답 성공
-
-            }.onFailure { // 응답 실패
-
+            val dateInfo = DateUtil.getDateInfo()
+            val today = DateUtil.getToday()
+            _state.update {
+                it.copy(
+                    weekInfo = dateInfo.last(),
+                    dayInfo = "${dateInfo[0][0]}.${dateInfo[1]}(월) ~ ${dateInfo[0][0]}.${dateInfo[7]}(일)"
+                )
             }
         }
+    }
+
+    // 마이데이터 연동
+    private fun getMyData() {
+        viewModelScope.launch {
+            val budgetData = BudgetUtil.getRandomMoney().toList()
+            delay(1000)
+            _state.update {
+                it.copy(
+                    isMyDataLinked = true,
+                    budget = budgetData,
+                    sumBudget = "총 ${budgetData.sum().toWon()}"
+                )
+            }
+        }
+    }
+
+    // 자산 설정
+    fun setBudgetInfo(budgetInfo: IntArray) {
+        viewModelScope.launch {
+            runCatching {
+                _result.emit(Result.Loading)
+                dataStoreRepository.setBudget(
+                    monday = budgetInfo[0],
+                    tuesday = budgetInfo[1],
+                    wednesday = budgetInfo[2],
+                    thursday = budgetInfo[3],
+                    friday = budgetInfo[4],
+                    saturday = budgetInfo[5],
+                    sunday = budgetInfo[6]
+                )
+            }.onSuccess { // 응답 성공
+                _result.emit(Result.Finish)
+                _action.emit(WeekBudgetAction.NavigateHomeView)
+            }.onFailure { // 응답 실패
+                _result.emit(Result.Finish)
+            }
+        }
+    }
+
+    fun navigateHomeFragment() = viewModelScope.launch {
+        _action.emit(WeekBudgetAction.ShowBudgetDialog)
     }
 }
 
 data class WeekBudgetState(
-    val dataSomething: String = "",
+    var weekInfo: String = "",
+    var dayInfo: String = "",
+    var isMyDataLinked: Boolean = false,
+    var budget: List<Int> = arrayListOf(),
+    var sumBudget: String = "",
 )
 
 sealed interface WeekBudgetAction {
     class ShowToast(val content: String) : WeekBudgetAction
+    data object ShowBudgetDialog : WeekBudgetAction
+    data object NavigateHomeView : WeekBudgetAction
 }
 
 sealed interface Result {
