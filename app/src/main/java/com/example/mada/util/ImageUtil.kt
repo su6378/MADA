96e3bf698b581f2de8ddb41f8ec2981b360e7a10
SooYun Jo <josooyeon11@naver.com>
2025-09-27 16:46:12 +0900

    [FEAT] 바인더 이미지 교체 애니메이션 추가

--- app/src/main/java/com/example/mada/util/ImageUtil.kt
+++ app/src/main/java/com/example/mada/util/ImageUtil.kt
@@ -1,6 +1,8 @@
 package com.example.mada.util
 
 import android.widget.ImageView
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 object ImageUtil {
     fun ImageView.changeImageWithFade(newImageRes: Int, duration: Long = 500) {
@@ -10,6 +12,24 @@ object ImageUtil {
             .withEndAction {
                 // 2️⃣ 이미지 교체
                 this.setImageResource(newImageRes)
+
+                // 3️⃣ fade in
+                this.animate()
+                    .alpha(1f)
+                    .setDuration(duration)
+                    .start()
+            }
+            .start()
+    }
+
+    fun ImageCarousel.changeItemWithFade(images: MutableList<CarouselItem>, position: Int, newImageRes: Int, duration: Long = 500) {
+        this.animate()
+            .alpha(0f)
+            .setDuration(duration)
+            .withEndAction {
+                // 2️⃣ 이미지 교체
+                images[position] = CarouselItem(newImageRes)
+                this.setData(images)
 
                 // 3️⃣ fade in
                 this.animate()
